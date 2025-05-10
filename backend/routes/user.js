80164f264f1ef6a6e2a9fd1e2ac3c148002b91c0
Maxamed459 <maxamedmahdi459@gmail.com>
2025-05-10 11:40:25 +0300

    add routes

---
+++ backend/routes/user.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { registerUser } from "../controller/userController.js";
+
+const userRouter = express.Router();
+
+userRouter.post("/register-user", registerUser);
+
+export default userRouter;
